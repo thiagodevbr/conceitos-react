e2ac1b5d61a858b134f1a5c6a7885407c01ff572
Thiago <thiago.brito@aluno.faculdadeimpacta.com.br>
2020-04-13 00:58:20 -0300

    Properties content

--- src/App.js
+++ src/App.js
@@ -2,5 +2,13 @@ import React from "react";
 import Header from "./components/Header";
 
 export default function App() {
-  return <Header />;
+  return (
+    <Header title="Conteudo de React">
+      <ul>
+        <li>Node</li>
+        <li>React</li>
+        <li>Native</li>
+      </ul>
+    </Header>
+  );
 }
